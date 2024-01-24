582774c8424007ad9064caa9d4b71a9bb6d87268
Kevin Bond <kevinbond@gmail.com>
2024-01-24 17:14:13 -0500

    feat: add option to download icon as svg

--- app/Http/Controllers/ShowIconController.php
+++ app/Http/Controllers/ShowIconController.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Icon;
-use Illuminate\View\View;
 
 final class ShowIconController
 {
-    public function __invoke(Icon $icon): View
+    public function __invoke(Icon $icon, bool $download = false)
     {
+        if ($download) {
+            return response(svg($icon->name)->toHtml())
+                ->header('Content-Type', 'image/svg+xml')
+                ->header('Content-disposition', sprintf('attachment; filename="%s.svg"', $icon->name));
+        }
+
         return view('blade-icons.show', [
             'icon' => $icon,
             'icons' => Icon::relatedIcons($icon),
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', WelcomeController::class)->name('home');
 Route::view('/blade-icons', 'blade-icons.index')->name('blade-icons');
+Route::get('/blade-icons/{icon}.svg', ShowIconController::class)->name('blade-icon-download')->defaults('download', true);
 Route::get('/blade-icons/{icon}', ShowIconController::class)->name('blade-icon');
 Route::get('/docs/{version?}/{page?}', DocsController::class)->name('docs');
